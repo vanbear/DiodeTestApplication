07246eba71a1f48fb62b59367f2d541138283c5f
Daniel Wąs <dbearb@gmail.com>
2018-11-13 15:29:17 +0100

    Easydriver

--- ArduinoComponent/DiodeTestApplication/DiodeTestApplication.ino
+++ ArduinoComponent/DiodeTestApplication/DiodeTestApplication.ino
@@ -1,69 +1,29 @@
-#include "Motor/MotorDriver.h"
+#include "Motor/MyMotor.h"
 
-float checkLightDensity()
-{
-  Serial.println("Light density");
-}
+Pins pins1(2, 3, 4, 5);
+MyMotor motor1(pins1, 200);
 
-bool calibrate()
-{
-    Serial.println("Calibration start.");
-    while (digitalRead(motor::HALL_IN_PIN)==HIGH)
-    {
-      motor::do16Steps(1, true);
-    }
-    Serial.println("Calibration end.");
-    delay(1000);
-    motor::motorOff();
-    return true;
-}
-
-void measure()
-{
-    motor::myStepper2.setSpeed(150);
-    int steps1 = 25;
-    int steps2 = 32;
-    for (int i=0; i<steps1; i++)
-    {
-      motor::do16Steps(1, true);
-      delay(100);
-      for (int j=0; j<steps2; j++)
-      {
-          Serial.println((String)i+":"+(String)j);
-          motor::myStepper2.step(2048/steps2);
-          delay(100);
-          //checkLightDensity();
-      }
-    }
-    motor::motorOff();
-    Serial.println("Koniec.");
-}
-
-void initPinModes()
-{
-pinMode(motor::MOTOR1_PIN_A, OUTPUT);     
-pinMode(motor::MOTOR1_PIN_B, OUTPUT);     
-pinMode(motor::MOTOR1_PIN_C, OUTPUT);     
-pinMode(motor::MOTOR1_PIN_D, OUTPUT);
-
-pinMode(motor::MOTOR2_PIN_A, OUTPUT);     
-pinMode(motor::MOTOR2_PIN_B, OUTPUT);     
-pinMode(motor::MOTOR2_PIN_C, OUTPUT);     
-pinMode(motor::MOTOR2_PIN_D, OUTPUT);
-
-pinMode(motor::HALL_IN_PIN, INPUT);
-}
+Pins pins2(8, 9, 10, 11);
+MyMotor motor2(pins2, 200);
 
 void setup()
 {
   Serial.begin(9600);
-  initPinModes();
-  if(calibrate())
-  {
-    measure();
-  }
+
+  motor1.setSpeed(1);
+  motor2.setStepMode(StepMode::FULL_STEP);
+  
+  motor2.setSpeed(3);
+  motor2.setStepMode(StepMode::FULL_STEP);
 }
 
-void loop() {
-// done in setup
+void loop() 
+{
+  motor2.doNSteps(200);
+  motor2.toggleDirection();
+  delay(800);
+
+  motor1.doNSteps(500);
+  motor1.toggleDirection();
+  delay(800);
 }
---
+++ ArduinoComponent/DiodeTestApplication/Motor/MyMotor.h
@@ -0,0 +1,129 @@
+enum class StepMode
+{
+    FULL_STEP = 0,
+    HALF_STEP,
+    QUARTER_STEP,
+    EIGHT_STEP
+};
+
+struct Pins
+{
+    Pins(int step, int dir, int MS1 = -1, int MS2 = -1) :
+        step(step), direction(dir), MS1(MS1), MS2(MS2)
+    {}
+    int step;
+    int direction;
+    int MS1;
+    int MS2;
+};
+
+class MyMotor
+{
+public:
+    MyMotor(Pins, unsigned int resolution, bool dir = false, 
+        StepMode mode = StepMode::FULL_STEP);
+    void doStep();
+    void doNSteps(int n);
+    void changeDirection(bool dir);
+    void toggleDirection();
+    void setSpeed(float spd);
+    void setStepMode(StepMode);
+
+private:
+    unsigned int m_resolution;
+    bool m_direction;
+    Pins m_pins;
+    float m_speed = 100;
+
+    void initPinModes();
+    bool isPinSet(int pin) const { return (pin > -1); }
+};
+
+MyMotor::MyMotor(Pins pins, unsigned int res, bool dir, StepMode mode) : m_pins(pins), m_resolution(res)
+{
+    initPinModes();
+    changeDirection(dir);
+    setSpeed(m_speed);
+    digitalWrite(m_pins.step, LOW);
+}
+
+void MyMotor::initPinModes()
+{
+    pinMode(m_pins.step, OUTPUT);
+    pinMode(m_pins.direction, OUTPUT);
+    if (isPinSet(m_pins.MS1))
+    {
+        pinMode(m_pins.MS1, OUTPUT);
+    }
+    if (isPinSet(m_pins.MS2))
+    {
+        pinMode(m_pins.MS2, OUTPUT);
+    }
+}
+    
+void MyMotor::changeDirection(bool dir)
+{
+    m_direction = dir;
+    digitalWrite(m_pins.direction, (m_direction ? HIGH : LOW));
+}
+
+void MyMotor::toggleDirection()
+{
+    m_direction = !m_direction;
+    changeDirection(m_direction);
+}
+
+void MyMotor::setSpeed(float spd)
+{
+    m_speed = spd;
+}
+
+void MyMotor::setStepMode(StepMode stepMode)
+{
+    if (!m_pins.MS1 and !m_pins.MS2)
+    {
+        return;
+    }
+
+    bool MS1 = false;
+    bool MS2 = false;
+    switch (stepMode)
+    {
+        case StepMode::FULL_STEP:
+        default:
+            MS1 = false;
+            MS2 = false;
+            break;
+        case StepMode::HALF_STEP:
+            MS1 = true;
+            MS2 = false;
+            break;
+        case StepMode::QUARTER_STEP:
+            MS1 = false;
+            MS2 = true;
+            break;
+        case StepMode::EIGHT_STEP:
+            MS1 = true;
+            MS2 = true;
+            break;
+    }
+    digitalWrite(m_pins.MS1, (MS1 ? HIGH : LOW));
+    digitalWrite(m_pins.MS2, (MS2 ? HIGH : LOW));
+}
+
+void MyMotor::doStep()
+{
+    // Serial.println(m_speed);
+    digitalWrite(m_pins.step,HIGH);
+    delay(m_speed);
+    digitalWrite(m_pins.step,LOW);
+    delay(m_speed);
+}
+
+void MyMotor::doNSteps(int n)
+{
+    for (int i = 0; i <= n; i++)
+    {
+        doStep();
+    }
+}
