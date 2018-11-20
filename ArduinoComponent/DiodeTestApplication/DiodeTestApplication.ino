c5a40d14ba47ca26d711ee7c4e82e4c102750eec
Daniel Wąs <dbearb@gmail.com>
2018-11-20 17:55:57 +0100

    Led, photoresistors nad second Hall sensor.

--- ArduinoComponent/DiodeTestApplication/DiodeTestApplication.ino
+++ ArduinoComponent/DiodeTestApplication/DiodeTestApplication.ino
@@ -13,7 +13,8 @@ void setup()
   Serial.begin(9600);
 
   // hall sensor
-  pinMode(global::pins::HALL_SENSOR, INPUT);
+  pinMode(global::pins::HALL_SENSOR_A, INPUT);
+  pinMode(global::pins::HALL_SENSOR_B, INPUT);
 
   // motors
   motorDiode.setSpeed(1);
@@ -30,7 +31,7 @@ void setup()
   }
 }
 
-void loop() 
+void loop()
 {
 }
 
@@ -39,49 +40,89 @@ bool calibrate()
   int stepsDone = 0;
   const int stepsPerRevolution = 1;
   motorBase.setSpeed(6);
+  motorBase.changeDirection(true);
   
   Serial.println("Calibration start.");
-  while (digitalRead(global::pins::HALL_SENSOR) == HIGH)
+  while ( (digitalRead(global::pins::HALL_SENSOR_A) == HIGH) && (digitalRead(global::pins::HALL_SENSOR_B) == HIGH) )
   {
     motorBase.doNSteps(stepsPerRevolution);
     stepsDone += stepsPerRevolution;
     if (stepsDone >= global::steps::MOTOR_BASE_MAX)
     {
       Serial.println("Calibration failed - Hall sensor didn't activate after full rotation, returning to starting position.");
-      motorBase.setSpeed(1);
+      motorBase.setSpeed(4);
       motorBase.toggleDirection();
       motorBase.doNSteps(stepsDone);
       return false;
     }
   }
   Serial.println("Calibration succesful.");
+  motorBase.doStep(); // last additional step
   delay(2000);
   return true;
+}
+
+void getLightDensity()
+{
+  int valueMain = analogRead(global::pins::LIGHT_SENSOR_MAIN);
+  int valueRef = analogRead(global::pins::LIGHT_SENSOR_REF);
+  // int valueMap = map(valueMain - valueRef, 0, 1023, 0, 255);
+  int valueMap = valueMain - valueRef;
+
+  Serial.print(" Value Main: "); Serial.print(valueMain);
+  Serial.print(" | Value Ref: "); Serial.print(valueRef);
+  Serial.print(" | Mapped Value: "); Serial.println(valueMap);
 }
 
 void measure()
 {
   Serial.println("Measure start.");
 
+  if ( digitalRead(global::pins::HALL_SENSOR_A) == LOW )
+  {
+    Serial.println("Sensor A - direction CCW.");
+    motorBase.changeDirection(false);
+  }
+  else if ( digitalRead(global::pins::HALL_SENSOR_B) == LOW )
+  {
+    Serial.println("Sensor B - direction CW.");
+    motorBase.changeDirection(true);
+  }
+  else
+  {
+    Serial.println("Cannot determine base motor direction.");
+    return;
+  }
+  
   int stepsDone_Base = 0;
   int stepsDone_Diode = 0;
+  int counterBase = 0;
+  int counterDiode = 0;
+  motorBase.setStepMode(StepMode::HALF_STEP);
 
   while (stepsDone_Base < global::steps::MOTOR_BASE_MAX)
   {
-    Serial.println("Base step");
     stepsDone_Diode = 0;
+    counterDiode = 0;
 
-    motorBase.doNSteps(global::steps::MOTOR_BASE_MEASURE_STEP);
-    stepsDone_Base += global::steps::MOTOR_BASE_MEASURE_STEP;
     while (stepsDone_Diode < global::steps::MOTOR_DIODE_MAX)
     {
-      Serial.println("Diode step");
+      Serial.print(counterBase); Serial.print(" : "); Serial.print(counterDiode);
+      getLightDensity();
+
       motorDiode.doNSteps(global::steps::MOTOR_DIODE_MEASURE_STEP);
       stepsDone_Diode += global::steps::MOTOR_DIODE_MEASURE_STEP;
 
+      counterDiode++;
       delay(global::time::DELAY_BETW_MEASURE_STEPS);
     }
+
+    counterBase++;
+    motorDiode.toggleDirection();
+    motorBase.doNSteps(global::steps::MOTOR_BASE_MEASURE_STEP);
+    stepsDone_Base += global::steps::MOTOR_BASE_MEASURE_STEP;
   }
 
+  getLightDensity(); 
   Serial.println("Measure end.");
 }
--- ArduinoComponent/DiodeTestApplication/GlobalDefs.h
+++ ArduinoComponent/DiodeTestApplication/GlobalDefs.h
@@ -2,16 +2,19 @@ namespace global
 {
     namespace pins
     {
-    constexpr auto HALL_SENSOR = 7;
+        constexpr auto HALL_SENSOR_A = 6;
+        constexpr auto HALL_SENSOR_B = 7;
+        constexpr int LIGHT_SENSOR_MAIN = A0;
+        constexpr int LIGHT_SENSOR_REF = A1;
     }
 
     namespace steps
     {
-    constexpr auto MOTOR_BASE_MAX = 200;
-    constexpr auto MOTOR_DIODE_MAX = 2048;
+        constexpr auto MOTOR_BASE_MAX = 200;
+        constexpr auto MOTOR_DIODE_MAX = 2048;
 
-    constexpr auto MOTOR_BASE_MEASURE_STEP = 2;
-    constexpr auto MOTOR_DIODE_MEASURE_STEP = 32;
+        constexpr auto MOTOR_BASE_MEASURE_STEP = 8;
+        constexpr auto MOTOR_DIODE_MEASURE_STEP = 64;
     }
 
     namespace time
