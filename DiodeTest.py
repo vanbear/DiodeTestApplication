b81142e2f165171c91cfe5cae3197f02c1b7f48c
Daniel Wąs <dbearb@gmail.com>
2018-12-04 17:25:08 +0100

    Matplotlib

--- DiodeTest.py
+++ DiodeTest.py
@@ -1,20 +1,30 @@
+import os
+import sys
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+import serial
+import serial.tools.list_ports
+from matplotlib.backends.backend_qt5agg import \
+    FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtCore import QThread, pyqtSignal, pyqtSlot
 from PyQt5.QtWidgets import QSizePolicy
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
-import sys
-import os
-import serial
-import serial.tools.list_ports
 
+#=========== GLOBAL VARIABLES
 curdir = os.path.dirname(os.path.abspath(__file__))
 startingNames = ["StartingPoint", "maxStepsA", "maxStepsB"]
 valueNames = ["stepA", "stepB", "dirA", "dirB", "value"]
 overallSteps = 0
 madeSteps = 0
+acquiredData = []
+previousMilis = 0
+plotInterval = 3000
 
+
+#=========== DATA TRANSFER
 class DataTransferThread(QThread):
     signalStatusBarUpdate = pyqtSignal('QString')
     signalStatusBarStyleNormal = pyqtSignal()
@@ -22,6 +32,7 @@ class DataTransferThread(QThread):
     signalAddValuesToTable = pyqtSignal(list)
     signalStateLabelSetText = pyqtSignal('QString')
     signalUpdateProgressBar = pyqtSignal(int)
+    signalUpdatePlot = pyqtSignal(list)
 
     def __init__(self):
         QThread.__init__(self)
@@ -33,9 +44,10 @@ class DataTransferThread(QThread):
         self.wait()
 
     def run(self):
-        global overallSteps, madeSteps
+        global overallSteps, madeSteps, acquiredData
         overallSteps = 0
         madeSteps = 0
+        acquiredData = []
         while True:
             if self.isPortAvailable:
                 try:
@@ -67,19 +79,25 @@ class DataTransferThread(QThread):
             self.parseError(msgValues)
 
     def parseStartingData(self, data):
-        global overallSteps
+        global overallSteps, acquiredData
         valuesList = data.split(b";")
         [startingPoint, maxA, maxB] = valuesList
         overallSteps = int(maxA)*int(maxB)
         self.signalStateLabelSetText.emit('Starting at point ' + startingPoint.decode() + ' with ' + str(overallSteps) + ' overall steps')
+        
 
     def parseData(self, data):
-        global madeSteps
+        global madeSteps, previousMilis, plotInterval
         self.signalStateLabelSetText.emit('Measuring (' + str(madeSteps) + ' / ' + str(overallSteps) + ')')
         valuesList = data.split(b";")
         madeSteps = madeSteps + 1
         self.signalUpdateProgressBar.emit(int(madeSteps/800*100))
         self.signalAddValuesToTable.emit(valuesList)
+        acquiredData.append(valuesList[-1].decode().replace('\r', ''))
+        currentMilis = getTime()
+        if (currentMilis - previousMilis > plotInterval):
+            previousMilis = currentMilis
+            self.signalUpdatePlot.emit(list(map(int, acquiredData)))
 
     def parseError(self, data):
         self.signalStateLabelSetText.emit(data.decode())
@@ -92,11 +110,36 @@ class DataTransferThread(QThread):
             self.isPortAvailable = False
             pass
 
+#=========== PLOT CANVAS
+class PlotCanvas(FigureCanvas):
+    def __init__(self, parent=None, width = 5, height = 4, dpi = 100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+ 
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+ 
+        FigureCanvas.setSizePolicy(self,
+                QSizePolicy.Expanding,
+                QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+        self.plot([])
+
+    def plot(self, data):
+        self.axes.clear()
+        self.axes.plot(data, 'r-')
+        self.draw()
+
+#=========== MAIN APP
 class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(ApplicationWindow, self).__init__()
+        self.setFixedSize(890,560)
+
         self.ui = uic.loadUi(os.path.join(curdir, 'DiodeTestGUI/mainwindow.ui'), self)
         self.setupTable()
+        self.plotCanvas = PlotCanvas(self.plotView)
+        self.plotCanvas.move(0,0)
         # == thread
         self.dataThread = DataTransferThread()
         self.setupSignals()
@@ -109,6 +152,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.dataThread.signalAddValuesToTable.connect(self.addTableItem)
         self.dataThread.signalStateLabelSetText.connect(self.setStateLabelText)
         self.dataThread.signalUpdateProgressBar.connect(self.updateProgressBar)
+        self.dataThread.signalUpdatePlot.connect(self.updatePlot)
 
     def setupTable(self):
         columnCount = 5
@@ -152,7 +196,11 @@ class ApplicationWindow(QtWidgets.QMainWindow):
     def setStateLabelText(self, value):
         self.ui.stateLabel.setText(value)
 
+    @pyqtSlot(list)
+    def updatePlot(self, values):
+        self.plotCanvas.plot(values)
 
+#=========== GLOBAL METHODS
 def getArduinoPort():
     arduino_ports = [
     p.device
@@ -163,6 +211,9 @@ def getArduinoPort():
         raise IOError("No Arduino found")
 
     return serial.Serial(arduino_ports[0], 9600)
+
+def getTime():
+    return int(round(time.time() * 1000))
 
 def main():
     # === INIT ===
